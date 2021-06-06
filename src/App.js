187e8ecf99e2dd44cf25c56a8392682415233321
Rafael <rafael.oelmann@gmail.com>
2021-06-06 10:59:18 +0100

    added sate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setUsername] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [favLang, setFavLang] = useState("");
+  const [error, seterror] = useState(null);
+
   return (
     <div>
       <h1>Hi rafa</h1>
