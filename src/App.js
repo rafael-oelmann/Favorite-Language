10fb20a5711422820ba83e4fea808cc27e207647
Rafael <rafael.oelmann@gmail.com>
2021-06-07 11:03:38 +0100

    refacrtored to use await

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ function App() {
   const [avatar, setAvatar] = useState("");
   const [favLang, setFavLang] = useState("");
   const [repos, setRepos] = useState("");
-  const [error, seterror] = useState(null);
+  const [userInput, setUserInput] = useState("");
+  const [error, setError] = useState(null);
+
+  const headers = {
+    access_token: "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",
+  };
 
   useEffect(() => {
-    fetch("https://api.github.com/users/example")
+    fetch(
+      "https://api.github.com/users/example" +
+        `?access_token=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`
+    )
       .then((res) => res.json())
       .then((data) => {
         setData(data);
@@ -24,42 +32,87 @@ function App() {
     setRepos(public_repos);
   };
 
+  const handleSearch = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    fetch(
+      `https://api.github.com/users/${userInput}` +
+        `?access_token=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`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+          setError(data.message);
+        } else {
+          setData(data);
+          mostUsedLang();
+          setError(null);
+        }
+      });
+  };
+
+  const mostUsedLang = async () => {
+    setFavLang("loading...");
+    const sumMap = {};
+    const userResponse = await fetch(
+      `https://api.github.com/users/${userInput}/repos?per_page=100` +
+        `?access_token=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`
+    );
+    const userRepos = await userResponse.json();
+    for (const repo of userRepos) {
+      const rawRepo = await fetch(
+        repo.languages_url +
+          `?access_token=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`
+      );
+      const repoReq = await rawRepo.json();
+      Object.keys(repoReq).forEach((language) => {
+        if (!!!sumMap[language]) sumMap[language] = 0;
+        sumMap[language] += repoReq[language];
+      });
+    }
+    const mostUsed = Object.keys(sumMap).reduce((a, b) =>
+      sumMap[a] > sumMap[b] ? a : b
+    );
+    setFavLang(mostUsed);
+  };
+
   return (
-    <div>
+    <div className="body">
       <div className="navbar">Favorite Programming Language</div>
       <div className="search">
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group>
-            <Form.Input placeholder="Github username" name="github username" />
+            <Form.Input
+              placeholder="Github username"
+              name="github username"
+              onChange={handleSearch}
+            />
 
             <Form.Button content="Search" />
           </Form.Group>
         </Form>
       </div>
-      <div className="card">
-        <Card>
-          <Image
-            src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-            wrapped
-            ui={false}
-          />
-          <Card.Content>
-            <Card.Header>Matthew</Card.Header>
-            <Card.Meta>
-              <span className="date">Joined in 2015</span>
-            </Card.Meta>
-            <Card.Description>
-              Matthew is a musician living in Nashville.
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name="user" />
-              22 Friends
-            </a>
-          </Card.Content>
-        </Card>
-      </div>
+      {error ? (
+        <h1>{error}</h1>
+      ) : (
+        <div className="card">
+          <Card>
+            <Image src={avatar} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>{name}</Card.Header>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <p>Favorite Programming Language:</p>
+                <Icon name="code" />
+                {favLang === "" ? "???" : favLang}
+              </a>
+            </Card.Content>
+          </Card>
+        </div>
+      )}
     </div>
   );
 }
